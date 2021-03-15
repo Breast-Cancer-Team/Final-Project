8d872e78ee0ad487e801bdd0250beac062a67988
Kevin Cao <kevincao222@gmail.com>
2021-03-15 16:28:36 -0700

    pep8 compliant now

--- Final-Project/clean_split_data.py
+++ Final-Project/clean_split_data.py
@@ -1,12 +1,11 @@
-import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
 def clean_data(data):
     '''
     Drops ID column of data as well as Not a Number column at end of dataset.
-    Retains all mean measurement columns and drops all standard errors and worst measurements.
+    Retains all mean measurement columns and drops all
+    standard errors and worst measurements.
     Remaps diagnosis column to numbered booleans.
     '''
     data.drop(list(data.filter(regex='_se')), axis=1, inplace=True)
@@ -15,7 +14,7 @@ def clean_data(data):
     data.drop(list(data.filter(regex='Unnamed: 32')), axis=1, inplace=True)
     data['diagnosis'].replace('B', 0, inplace=True)
     data['diagnosis'].replace('M', 1, inplace=True)
-    
+
     return data
 
 
@@ -24,13 +23,15 @@ def split_data(data):
     Takes cleaned dataset and splits into train, test sets.
     '''
     X = data[['radius_mean', 'texture_mean', 'perimeter_mean',
-       'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean',
-       'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean']]
+              'area_mean',
+              'smoothness_mean',
+              'compactness_mean',
+              'concavity_mean',
+              'concave points_mean',
+              'symmetry_mean',
+              'fractal_dimension_mean']]
     y = data.diagnosis
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
+    X_train, X_test, y_train, y_test =
+    train_test_split(X, y, test_size=0.2, random_state=42)
+
     return X_train, X_test, y_train, y_test
-
-
-
-
--- Final-Project/gradient_boosting.py
+++ Final-Project/gradient_boosting.py
@@ -1,29 +1,25 @@
+# Cleaing and splitting data
+from clean_split_data import clean_data
+from clean_split_data import split_data
+
+
 # Pandas library for the pandas dataframes
-import pandas as pd    
-import numpy as np 
+import pandas as pd
+
 
 # Import Scikit-Learn library for the regression models
-import sklearn         
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import r2_score, mean_squared_error, accuracy_score
-from sklearn.feature_selection import f_regression, SequentialFeatureSelector
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix 
 from sklearn.metrics import plot_confusion_matrix
 
 # Import plotting libraries
 import seaborn as sns
-import matplotlib 
-from matplotlib import pyplot as plt
+import matplotlib
+
 
 # Set larger fontsize for all plots
 matplotlib.rcParams.update({'font.size': 20})
 
-from clean_split_data import clean_data
-from clean_split_data import split_data
 
 # ### Data
 data = pd.read_csv('data/data.csv')
@@ -32,17 +28,22 @@ X_train, X_test, y_train, y_test = split_data(data)
 
 # ### Classifier
 tree_count = 10
-gradient_model = GradientBoostingClassifier(n_estimators=tree_count, learning_rate=0.1,max_depth=10,random_state=42)
-gradient_model.fit(X_train,y_train)
+gradient_model = GradientBoostingClassifier
+(n_estimators=tree_count,
+ learning_rate=0.1,
+ max_depth=10, random_state=42)
+gradient_model.fit(X_train, y_train)
+
 
 # ### Optimized Gradient Boosting Predictor
 def feature_names():
     '''
-    Returns array of input features of best performing backwards stepwise selection test.
+    Returns array of input features of best
+    performing backwards stepwise selection test.
     '''
-    
+
     return ['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean',
-             'concavity_mean', 'concave points_mean', 'symmetry_mean']
+            'concavity_mean', 'concave points_mean', 'symmetry_mean']
 
 
 def predict(test_data):
@@ -51,9 +52,11 @@ def predict(test_data):
     '''
     X = data[feature_names()]
     y = data.diagnosis
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    gradient_model = GradientBoostingClassifier(n_estimators=10, learning_rate=0.1,max_depth=10, random_state=42)
-    gradient_model.fit(X_train,(y_train))
+    X_train, X_test, y_train, y_test =
+    train_test_split(X, y, test_size=0.2, random_state=42)
+    gradient_model = GradientBoostingClassifier
+    (n_estimators=10, learning_rate=0.1, max_depth=10, random_state=42)
+    gradient_model.fit(X_train, (y_train))
     y_pred = gradient_model.predict(test_data)
-    
+
     return y_pred
